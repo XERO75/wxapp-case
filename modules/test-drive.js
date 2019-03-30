03382fc0684d4be100ceedcf2f4523732f250871
xero75 <xerolry@gmail.com>
2019-03-30 15:34:50 +0800

    完成微信小程序-案例故事列表：视频

---
+++ modules/test-drive.js
@@ -0,0 +1,8 @@
+const testDrive = () => {
+  wx.showToast({
+    title: '暂不支持'
+  })
+}
+
+
+export default testDrive
--- pages/stories/index.js
+++ pages/stories/index.js
@@ -1,29 +1,37 @@
+const app = getApp()
+
 Page({
     data: {
-        
+        entities: null
+    },
+    play(event){
+      const currentVideo = wx.createVideoContext(`${event.target.dataset.vid}`)
+      currentVideo.play()
     },
     onLoad: function(options) {
         //Do some initialize when page load.
-        
+        this.setData({
+          entities: app.globalData.stories
+        })
     },
     onReady: function() {
         //Do some when page ready.
-        
+
     },
     onShow: function() {
         //Do some when page show.
-        
+
     },
     onHide: function() {
         //Do some when page hide.
-        
+
     },
     onUnload: function() {
         //Do some when page unload.
-        
+
     },
     onPullDownRefresh: function() {
         //Do some when page pull down.
-        
+
     }
 })
